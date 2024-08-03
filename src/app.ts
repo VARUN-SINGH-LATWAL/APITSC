f985155755c7cc1ec0d27a208c43a95296ab0289
varunsingh <varunsingh0456@gmail.com>
2024-08-03 11:35:49 +0530

    crate user router

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import glovalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./users/userRouter";
 const app = express()
 
-app.get("/",(req,res,next)=>{
-
-
+app.get("/",(req,res)=>{
     res.json({message:"welcom to api"})
 })
 
+app.use("/api/users",userRouter)
 
 // GLOVAL ERROR HANDLER
 app.use(glovalErrorHandler)
