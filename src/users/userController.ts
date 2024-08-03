abb17f905f0e69400aa5e16cf6a5c2da7fcd8875
varunsingh <varunsingh0456@gmail.com>
2024-08-03 12:04:17 +0530

    add user modal

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (
   req: Request,
@@ -11,6 +12,12 @@ const createUser = async (
     if (!name || !email || !password) {
         const error = createHttpError(400,"All fields are required");
         return next(error)
+    }
+    //Database call
+    const user = await userModel.findOne({email:email})
+    if (user) {
+        const errro = createHttpError(400,"User already exists with this gmail")
+        return next(errro);
     }
     //Process
     //Response
