5835a673289cb6364be9e1742ef14bced906634d
varunsingh <varunsingh0456@gmail.com>
2024-08-03 15:16:46 +0530

    add jwt

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -3,7 +3,7 @@ import { NextFunction ,Request , Response } from "express";
 import { HttpError } from "http-errors";
 
 // GLOVAL ERROR HANDLER
- const glovalErrorHandler = (err:HttpError ,req: Request,res: Response,next: NextFunction)=>{
+ const glovalErrorHandler = (err:HttpError ,req: Request,res: Response, next: NextFunction)=>{
     const statusCode = err.statusCode || 500;
     return res.status(statusCode).json({
         message: err.message,
--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,40 +1,55 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
-const createUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-    const {name, email, password} = req.body;
-    //Validation
-    if (!name || !email || !password) {
-        const error = createHttpError(400,"All fields are required");
-        return next(error)
-    }
-    //Database call
-    const user = await userModel.findOne({email:email})
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+  //Validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+  //Database call
+  try {
+    const user = await userModel.findOne({ email: email });
+
     if (user) {
-        const errro = createHttpError(400,"User already exists with this gmail")
-        return next(errro);
+      const errro = createHttpError(400, "User already exists with this gmail");
+      return next(errro);
     }
+  } catch (err) {
+    return next(createHttpError(500, "Error While getting user"));
+  }
 
-    // Password -->> hash
-    const hashedPassword = await bcrypt.hash(password,10) ;
-    const newUser = userModel.create({
-        name,
-        email,
-        password: hashedPassword,
-    })
+  // Password -->> hash
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  let newUser: User;
+  try {
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (err) {
+    return next(createHttpError(500, "Error While getting user"));
+  }
+
+  try {
     //Token ganeration JWT
-     const token = sign({sub: (await newUser)._id },config.jwtSecret as string,{expiresIn:'7d',});
-   
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
     //Response
-    res.json({accessToken: token});
+    res.json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError(500, "Error While Signing the JWT token"));
+  }
 };
 
 export { createUser };
