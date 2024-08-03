bead3030b77a792da683e53a04c6eb9788865f66
varunsingh <varunsingh0456@gmail.com>
2024-08-03 11:49:03 +0530

    add user validation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import glovalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/userRouter";
 const app = express()
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.json({message:"welcom to api"})
--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-    res.json({message:"crate user"})
+    const {name, email, password} = req.body;
+    //Validation
+    if (!name || !email || !password) {
+        const error = createHttpError(400,"All fields are required");
+        return next(error)
+    }
+    //Process
+    //Response
+    res.json({message:"crate user"});
 };
 
 export { createUser };
